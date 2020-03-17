badb93843fca36829c06c11c9b097b20edc3586a
joshua kim <j0shxone@gmail.com>
2020-03-17 17:07:33 -0400

    created votes model.

---
+++ db/migrate/20200317035609_create_votes.rb
@@ -0,0 +1,11 @@
+class CreateVotes < ActiveRecord::Migration[5.2]
+  def change
+    create_table :votes do |t|
+      t.reference :account_id
+      t.reference :post
+      t.boolean :upvote
+      t.boolean :downvote
+      t.timestamp
+    end
+  end
+end
