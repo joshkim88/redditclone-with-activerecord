f2a53664ea796ce3a579613f71d5dfa538eb8cb5
Joshua <j0shxone@gmail.com>
2020-03-17 17:09:14 -0400

    Update 20200317035609_create_votes.rb

--- db/migrate/20200317035609_create_votes.rb
+++ db/migrate/20200317035609_create_votes.rb
@@ -1,7 +1,7 @@
 class CreateVotes < ActiveRecord::Migration[5.2]
   def change
     create_table :votes do |t|
-      t.reference :account_id
+      t.reference :account
       t.reference :post
       t.boolean :upvote
       t.boolean :downvote
