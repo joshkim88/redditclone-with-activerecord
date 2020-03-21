2323c952bad09df4221ea7fba7e3077b62a9b6ec
joshua kim <j0shxone@gmail.com>
2020-03-21 01:47:40 -0400

    Made profile view.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'home/index'
   devise_for :accounts
+  get "u/:username" => "public#profile", as: :profile
 
   resources :subreddits do
     resources :posts
